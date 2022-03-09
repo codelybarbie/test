9152d9bb5aae4facfcffa4ea5bd38e4570af9670
sarangcoding <pinklybarbie@gmail.com>
2022-03-09 17:41:39 +0900

    new practice udemy

--- test.java
+++ test.java
@@ -1,2 +1,16 @@
-// test comment 
-// asdjkhfasdhf
+public class test {
+    public static void main(String[] args){
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+
+    }
+
+    public static int calculateScore(String playerName, int score){ 
+        System.out.println("Player " + playerName + " score " + score + " points");  
+    return score * 1000;
+    }
+    
+
+}
+
+ 
