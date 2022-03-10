31c4e192d12b28113c007937f076fe2336df5464
sarangcoding <pinklybarbie@gmail.com>
2022-03-10 18:49:07 +0900

    practice method

--- test.java
+++ test.java
@@ -2,14 +2,26 @@ public class test {
     public static void main(String[] args){
         int newScore = calculateScore("Tim", 500);
         System.out.println("New score is " + newScore);
+        calculateScore(75); 
+        calculateScore();
 
     }
 
     public static int calculateScore(String playerName, int score){ 
-        System.out.println("Player " + playerName + " score " + score + " points");  
+        System.out.println("Player " + playerName + " scored " + score + " points");  
     return score * 1000;
     }
     
+
+    public static int calculateScore(int score){ 
+        System.out.println("UnNamed Player scored " + score + " points");  
+    return score * 1000;
+    }
+
+    public static int calculateScore(){ 
+        System.out.println("No player name, no player score.");  
+    return 0;
+    }
 
 }
 
